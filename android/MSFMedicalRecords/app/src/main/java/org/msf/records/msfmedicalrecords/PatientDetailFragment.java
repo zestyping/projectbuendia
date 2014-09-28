e968e99f163a8ae2779239c3882746d7cdd42292
Gil Julio <gilsjulio@gmail.com>
2014-09-28 10:52:19 +0100

    Overall basic layout sorted

---
+++ android/MSFMedicalRecords/app/src/main/java/org/msf/records/msfmedicalrecords/PatientDetailFragment.java
@@ -0,0 +1,68 @@
+package org.msf.records.msfmedicalrecords;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import org.msf.records.msfmedicalrecords.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Patient detail screen.
+ * This fragment is either contained in a {@link PatientListActivity}
+ * in two-pane mode (on tablets) or a {@link PatientDetailActivity}
+ * on handsets.
+ */
+public class PatientDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public PatientDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_patient_detail, container, false);
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.patient_detail, menu);
+    }
+}
